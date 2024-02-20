9554388687ed209ca89858b90c7aa4ffcca4f545
gatsuweb <ivan.duran@outlook.fr>
2024-02-20 17:40:55 +0100

    express 1.2

---
+++ src/controllers/usersControllers.js
@@ -0,0 +1,36 @@
+const database = require("../../database");
+
+const getUsers = (req, res) => {
+  database
+    .query("SELECT * FROM users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+    
+  database
+    .query("SELECT * FROM users WHERE id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.status(200).json(users[0]);
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUsersById,
+};
