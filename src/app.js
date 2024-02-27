f2b7cbc1ba6421f48a2de955cb215f1dc498bb8c
gatsuweb <ivan.duran@outlook.fr>
2024-02-27 11:37:47 +0100

    quetes put terminado

--- src/app.js
+++ src/app.js
@@ -9,9 +9,15 @@ const usersControllers = require("./controllers/usersControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.post("/api/movies", movieControllers.postMovie);
+app.put("/api/movies/:id", movieControllers.updateMovie);
+
 app.get("/api/users", usersControllers.getUsers);
 app.get("/api/users/:id", usersControllers.getUsersById);
-app.post("/api/movies", movieControllers.postMovie);
 app.post("/api/users", usersControllers.postUsers);
+app.put("/api/users/:id", usersControllers.updateUser);
+
+
+
 
 module.exports = app;
