8bbe0798cc517e59de95d8e559c46c8dfcf4ef22
gatsuweb <ivan.duran@outlook.fr>
2024-02-27 11:58:28 +0100

    quetes 04 delete terminado

--- src/app.js
+++ src/app.js
@@ -11,11 +11,13 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", movieControllers.postMovie);
 app.put("/api/movies/:id", movieControllers.updateMovie);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
 
 app.get("/api/users", usersControllers.getUsers);
 app.get("/api/users/:id", usersControllers.getUsersById);
 app.post("/api/users", usersControllers.postUsers);
 app.put("/api/users/:id", usersControllers.updateUser);
+app.delete("/api/users/:id", usersControllers.deleteUser);
 
 
 
